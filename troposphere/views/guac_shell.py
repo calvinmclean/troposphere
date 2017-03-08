566bf841ea25eb33ce29b25b9c83fd606e1cbc52
Calvin Mclean <calvinmclean@email.arizona.edu>
2017-03-08 15:20:27 -0700

    remove passwd var

--- troposphere/views/guac_shell.py
+++ troposphere/views/guac_shell.py
@@ -17,7 +17,6 @@ conn_id = str(uuid.uuid4())
 base64_conn_id = base64.b64encode(conn_id[2:] + "\0" + 'c' + "\0" + 'hmac')
 
 timestamp = int(round(time.time()*1000))
-passwd = 'display'
 
 def guac_shell(request):
 
@@ -28,7 +27,7 @@ def guac_shell(request):
             ip_address = request.POST['ipAddress']
             atmo_username = request.session.get('username','')
 
-            message = str(timestamp) + 'ssh' + ip_address + '22' + atmo_username + passwd
+            message = str(timestamp) + 'ssh' + ip_address + '22' + atmo_username
             signature = hmac.new(secret, message, hashlib.sha256).digest().encode("base64").rstrip('\n')
 
             request_string = ('timestamp=' + str(timestamp)
