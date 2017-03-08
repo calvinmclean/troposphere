fd6534861c9679980e2a9d25b87c1c9a06c54d0f
Calvin Mclean <calvinmclean@email.arizona.edu>
2017-03-08 10:47:34 -0700

    change url

--- troposphere/views/web_desktop.py
+++ troposphere/views/web_desktop.py
@@ -16,27 +16,26 @@ from django.template import RequestContext
 
 from itsdangerous import Signer, URLSafeTimedSerializer
 
-guac_server = 'http://192.168.0.60:8080/guacamole'
+guac_server = 'http://128.196.64.144:8080/guacamole'
 SECRET_KEY = 'secret'
+
+# Create UUID for connection ID
+conn_id = str(uuid.uuid4())
+base64_conn_id = base64.b64encode(conn_id[2:] + "\0" + 'c' + "\0" + 'hmac')
+
+timestamp = int(round(time.time()*1000))
+passwd = 'display'
 
 def _should_redirect():
     return settings.WEB_DESKTOP['redirect']['ENABLED']
 
 def web_desktop(request):
-    template_params = {}
 
     if request.user.is_authenticated():
 
         if 'ipAddress' in request.POST:
             ip_address = request.POST['ipAddress']
-            client_ip = request.META['REMOTE_ADDR']
-
-            # Create UUID for connection ID
-            conn_id = str(uuid.uuid4())
-            base64_conn_id = base64.b64encode(conn_id[2:] + "\0" + 'c' + "\0" + 'hmac')
-            timestamp = int(round(time.time()*1000))
-            passwd = 'display'
-            atmo_username = 'calvinmclean'
+            atmo_username = request.session.get('username','')
 
             message = str(timestamp) + 'vnc' + ip_address + '5902' + atmo_username + passwd
             signature = hmac.new(SECRET_KEY, message, hashlib.sha256).digest().encode("base64").rstrip('\n')
@@ -52,7 +51,7 @@ def web_desktop(request):
             request_response = requests.post(guac_server + '/api/tokens', data=request_string)
             token = json.loads(request_response.content)['authToken']
 
-            redirect_url = 'http://192.168.0.60:8080/guacamole/#/client/' + base64_conn_id + '?token=' + token
+            redirect_url = guac_server + '/#/client/' + base64_conn_id + '?token=' + token
             response = HttpResponseRedirect(redirect_url)
 
             return response
