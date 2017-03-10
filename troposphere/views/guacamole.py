a7b9946198d1ee0ab9ab7a996407bcdad511907d
Calvin Mclean <calvinmclean@email.arizona.edu>
2017-03-10 14:33:13 -0700

    reorganized a little bit and added variables to local.py.j2

--- troposphere/views/guacamole.py
+++ troposphere/views/guacamole.py
@@ -6,38 +6,49 @@ import hashlib
 import base64
 import requests
 
+from django.conf import settings
 from django.core.exceptions import PermissionDenied
 from django.http import HttpResponse, HttpResponseRedirect
 
-guac_server = 'http://128.196.64.157:8080/guacamole'
-secret = 'secret'
+guac_server = settings.GUACAMOLE['SERVER_URL']
+secret = settings.GUACAMOLE['SECRET_KEY']
 
-# Create UUID for connection ID
-conn_id = str(uuid.uuid4())
-base64_conn_id = base64.b64encode(conn_id[2:] + "\0" + 'c' + "\0" + 'hmac')
-
-timestamp = int(round(time.time()*1000))
 
 def guacamole(request):
 
     if request.user.is_authenticated():
 
+        # Create UUID for connection ID
+        conn_id = str(uuid.uuid4())
+        base64_conn_id = base64.b64encode(conn_id[2:] + "\0" + 'c' + "\0" + 'hmac')
+
+        # Create timestamp that looks like: 1489181545018
+        timestamp = str(int(round(time.time()*1000)))
+
         if 'ipAddress' in request.POST and 'protocol' in request.POST:
 
+            # Get IP, protocol, and username from request that was sent from button click
             ip_address = request.POST['ipAddress']
             protocol = request.POST['protocol']
             atmo_username = request.session.get('username','')
 
+            # Change some parameters depending on SSH or VNC protocol
+            # Note: passwd is hardcoded to 'display'. This doesn't seem ideal but it is
+            #       done the same in web_desktop.py
             port = '5901'
             passwd = 'display'
             if protocol == 'ssh':
                 port = '22'
                 passwd = ''
 
-            message = str(timestamp) + protocol + ip_address + port + atmo_username + passwd
+            # Concatenate info for a message
+            message = timestamp + protocol + ip_address + port + atmo_username + passwd
+
+            # Hash the message into a signature
             signature = hmac.new(secret, message, hashlib.sha256).digest().encode("base64").rstrip('\n')
 
-            request_string = ('timestamp=' + str(timestamp)
+            # Build the POST request
+            request_string = ('timestamp=' + timestamp
                               + '&guac.port=' + port
                               + '&guac.username=' + atmo_username
                               + '&guac.password=' + passwd
@@ -46,13 +57,11 @@ def guacamole(request):
                               + '&guac.hostname=' + ip_address
                               + '&id=' + conn_id)
 
-            # Send request and record the result
+            # Send request to Guacamole backend and record the result
             request_response = requests.post(guac_server + '/api/tokens', data=request_string)
             token = json.loads(request_response.content)['authToken']
 
-            response = HttpResponseRedirect(guac_server + '/#/client/' + base64_conn_id + '?token=' + token)
-
-            return response
+            return HttpResponseRedirect(guac_server + '/#/client/' + base64_conn_id + '?token=' + token)
         else:
             raise UnreadablePostError
 
