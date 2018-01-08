e2c1c635bfec97285539bd5cb26ee28f1f76eaae
Calvin Mclean <calvinmclean@email.arizona.edu>
2018-01-08 15:38:14 -0700

    Improve report_image view

--- troposphere/views/report_image.py
+++ troposphere/views/report_image.py
@@ -1,5 +1,6 @@
 import time
 import requests
+import logging
 
 logger = logging.getLogger(__name__)
 
@@ -11,14 +12,16 @@ def report_image(request):
         raise PermissionDenied
 
     # Check that all parameters are received
-    if not all(field in request.POST for field in ['image_id', 'username'])
+    if not all(field in request.POST for field in ['image_id', 'username']):
         return failure_response(
                 status.HTTP_400_BAD_REQUEST,
-                "POST does not contain the required data")
+                "POST does not contain the required data (image_id, username)")
+
+    auth_token = request.session.get('access_token')
 
     # data to send to Atmosphere
-    destination = \
-        "{api_root}/report_image/{image_id}?username={username}&timestamp={timestamp}" \
+    report_image_route = \
+        "{api_root}/image_report/{image_id}?username={username}&timestamp={timestamp}" \
         .format(
             api_root=settings.API_V2_ROOT,
             image_id=request.POST['image_id'],
@@ -26,5 +29,25 @@ def report_image(request):
             timestamp=time.time()
         )
 
-    # Send data to Atmosphere
-    return requests.get(destination)
+    headers = {
+        'Authorization': "Token %s" % auth_token,
+        'Accept': 'application/json',
+    }
+
+    response = requests.get(report_image_route, headers=headers, verify=False)
+
+    try:
+        # Raise exceptions for HTTP errors
+        response.raise_for_status()
+    except HTTPError as exc:
+        status_code = exc.response.status_code
+        if status_code == 404:
+            return failure_response(
+                    status.HTTP_404_NOT_FOUND,
+                    "image %s not found " % request.POST['image_id'])
+
+        logger.exception("Failed to report image using Atmosphere API: Response: %s", response)
+
+        return failure_response(
+                status.HTTP_503_SERVICE_UNAVAILABLE,
+                "Failed to report image. Please retry your request.")
